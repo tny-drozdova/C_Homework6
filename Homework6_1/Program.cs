ea5a89a9a21881a4f0c4f5b1dcc85566d2538ee6
Tanya Drozdova <tny.drozdova@gmail.com>
2023-07-06 13:49:40 -0400

    Adding Homework 6, tasks 1 and 2

---
+++ Homework6_1/Program.cs
@@ -0,0 +1,77 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//  Examples:   0, 7, 8, -2, -2 -> 2
+//              1, -7, 567, 89, 223-> 3
+
+
+// Firts desicion:
+Console.Write("Enter the number of values: ");
+//string value = Console.ReadLine();
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Enter value {i + 1}: ");
+    int number = Convert.ToInt32(Console.ReadLine());       
+
+    if (number > 0)
+        count++;
+}
+
+Console.WriteLine($"The count of numbers greater than 0 is: {count}");
+
+Console.ReadLine();
+
+
+
+/* 
+
+// The IDEAL desicion:
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+int[] GenerateArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($" Enter #{i + 1} element: "); //the number will be inclusive of min and max to that element
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+    System.Console.WriteLine($"element[{i}] = {array[i]}");
+}
+}
+
+int CountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+int lenght = Prompt ("Enter a number of elements > ");
+int [] array;
+array = GenerateArray(lenght);
+PrintArray(array);
+System.Console.WriteLine($"The munber of elements larger than 0 is {CountPositiveNum(array)}");
+
+*/
---
+++ Homework6_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// Examples: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Enter the values for the first line (y = k1 * x + b1):");
+double k1 = GetUserInput("k1");
+double b1 = GetUserInput("b1");
+
+Console.WriteLine("Enter the values for the second line (y = k2 * x + b2):");
+double k2 = GetUserInput("k2");
+double b2 = GetUserInput("b2");
+
+if (k1 == k2)
+{
+    Console.WriteLine("The lines are parallel and do not intersect.");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);  //The formula find the coordinates of X;
+    double y = k1 * x + b1;   //The formula find the coordinates of Y;
+    Console.WriteLine($"The point of intersection is ({x}; {y})");
+}
+
+
+double GetUserInput(string variableName)
+{
+    Console.Write($"Enter the value for {variableName}: ");
+    string input = Console.ReadLine();
+    double value;
+
+    while (!double.TryParse(input, out value))  //We converting and passing a string (input) into a double (value) by using 'OUT' parameter.
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+        Console.Write($"Enter the value for {variableName}: ");
+        input = Console.ReadLine();
+    }
+    return value;
+}
+
